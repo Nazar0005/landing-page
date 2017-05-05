9a0869fbbc843f322b52ce8df2e9ccdb8adfdb78
Nazar0005 <Nazar_49_@mail.ru>
2017-05-05 20:27:24 +0300

    jstoggle

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,15 @@
 
 // document.addEventListener("DOMContentLoaded", namFunction); вызов функцию после загрузки DOM
 
+document.querySelector('.user-account__sign').addEventListener('click', function () {
+    var div = document.querySelector('.hidden-user');
+
+    if (div.style.maxHeight) {
+        div.style.maxHeight = null;
+    } else {
+        div.style.maxHeight = div.scrollHeight + 'px';
+    }
+});
+
+
 
