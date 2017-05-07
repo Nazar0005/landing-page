01d88f80af69bb6c799e7be61ccba8676ec8da8e
Nazar0005 <Nazar_49_@mail.ru>
2017-05-07 18:40:20 +0300

    frank

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,36 @@
 'use strict';
 
 // document.addEventListener("DOMContentLoaded", namFunction); вызов функцию после загрузки DOM
-var x = function () {
-    
+
+
+// Функция Радном Число
+function randomSize(size) {
+  return Math.floor(Math.random() * size);
 }
-document.querySelector('.user-account__sign').addEventListener('click', function () {
- 
-});
 
+// Сохраняем случайные кординаты сундука
+var cord = {
+  width: randomSize(1400),
+  height: randomSize(260)
+};
 
+document.getElementById('game').addEventListener('click', startGame, false);
+
+console.log(cord.width, cord.height);
+
+function startGame(e) {
+  var x = e.clientX;
+  var y = e.clientY;
+
+  if (x > cord.width) {
+    console.log(x);
+   var resultWidth = x - cord.width;
+    console.log("Правильная ширина в районе" + resultWidth + "px");
+  } else {
+    console.log(y);
+    var resultHeight = x - cord.height;
+    console.log("Правильная высота в районе" + resultHeight + "px");
+  }
+
+}
 
